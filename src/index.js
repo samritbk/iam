23a1556069dca96697208f31ad8109162202fac9
Beraki <drogba20020@gmail.com>
2018-12-04 20:41:21 -0600

    Added registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import './index.css';
 
@@ -7,3 +8,4 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+registerServiceWorker();
